dabf4cde61f1a2c144c6861f0a2a9a345b4e06d5
werckerbot <pleasemailus@wercker.com>
2019-05-23 13:45:15 +0000

    [ci skip] deploy from

--- assets/js/auie.js
+++ assets/js/auie.js
@@ -6,7 +6,6 @@ if ( -1 != window.location.host.search( /creative-tweet.net/i ) ) {
 		s.async=1;
 		s.src=u;if(f){s.id=f;}e.parentNode.insertBefore(s,e);}};
 		// a('//www.google-analytics.com/analytics.js','ga');
-		a("https://apis.google.com/js/plusone.js");
 		a("//b.st-hatena.com/js/bookmark_button_wo_al.js");
 		a("//platform.twitter.com/widgets.js","twitter-wjs");
 		a("//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.4&appId=250299701647029","facebook-jssdk");
